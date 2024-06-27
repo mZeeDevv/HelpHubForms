7968da18f6c889cf78481cf834cd6cc59e942197
Hamza Khattak <hamzeekhattak56@gmail.com>
2024-06-27 15:07:38 +0500

    Added react router, Toaster and Favicon

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import logotwo from './assets/logot.png'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+function App() {
+
+  return (
+    <>
+      <img width={130} src={logotwo} alt="logo" />
+     
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  )
+}
+
+export default App
